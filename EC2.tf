ef403302ca09e8ee1f6a564c2d6843ac1242355f
VishnuKM7 <144899903+VishnuKM7@users.noreply.github.com>
2024-03-28 15:08:11 +0530

    Update EC2.tf
    Changed AMI to RHEL

--- EC2.tf
+++ EC2.tf
@@ -5,6 +5,6 @@ provider "aws" {
 
 # Create an AWS EC2 Instance
 resource "aws_instance" "example" {
-  ami           = "ami-05295b6e6c790593e"  # Specify the AMI ID of the desired Amazon Linux AMI
+  ami           = "ami-05a5bb48beb785bf1"  # Specify the AMI ID of the desired Amazon Linux AMI
   instance_type = "t2.micro"               # Specify the instance type as t2.micro
 }
